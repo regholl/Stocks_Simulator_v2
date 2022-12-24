6d26218ce89a0d0096613e2f0be4db8656360b61
perchik_air <1919ars@gmail.com>
2022-12-24 22:34:33 +0200

    more

--- pages/streamlit_concepts.py
+++ pages/streamlit_concepts.py
@@ -1,6 +1,6 @@
 import streamlit as st
-# import matplotlib.pyplot as plt
-# import plotly.express as px
+import matplotlib.pyplot as plt
+import plotly.express as px
 import pandas as pd
 import numpy as np
 import time
